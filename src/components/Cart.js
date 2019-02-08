9c6a7d6440bd3aa6ee1abcd22cdc928eebf92d0c
Jonghun Lee <jonghunjhl@gmail.com>
2019-02-08 15:27:42 -0500

    Reset function works now. removed state from Counter component which only renders values from Cart component

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -6,12 +6,17 @@ import { Button } from 'semantic-ui-react';
 
 class Cart extends Component {
     state = {
+        loading: 0,
         counters: [
             { id: 1, value: 0 },
             { id: 2, value: 0 },
             { id: 3, value: 0 } 
         ]
     }
+
+    // componentDidMount() {
+    //     const url = 'http://10.2.204.26:3000/';
+    // }
 
     handleDelete = (counterId) => {
         const counters = this.state.counters.filter((c) => c.id !== counterId);
@@ -24,23 +29,58 @@ class Cart extends Component {
         this.setState({ counters: [...this.state.counters, { id: newId, value: 0}] })
     }
 
+    handleReset = () => {
+        const counters = this.state.counters.map((counter) => {
+            counter.value = 0;
+            return counter;
+        });
+ 
+        this.setState({ counters });
+    }
+
+    handleIncrement = (counter) => {
+        const counters = [...this.state.counters];
+        const index = counters.indexOf(counter);
+        counters[index] = {...counter};
+        counters[index].value++;
+
+        this.setState({ counters });
+    }
+
     styles = {
         margin: '30px',
         padding: '100px'
     }
 
     render() {
-        return(
-            <div>
-                <Header>Total Number</Header>
-                <Button basic color='black' content='Reset' />
-                <Button basic color='green' content='Add' onClick={this.handleAdd} />
-                {
-                    this.state.counters.length === 0 ? "Empty" : this.state.counters.map(counter => (
-                        <Counter value={counter.value} key={counter.id} id={counter.id} onDelete={this.handleDelete} />
-                    ))
-                }
-            </div>
+        return (
+                <div>
+                    {this.state.loading ? <div>loading...</div> : 
+                        <div>
+                        <Header>Total Number</Header>
+                        <Button 
+                            onClick={this.handleReset}
+                            basic color='black'
+                            content='Reset'
+                        />
+                        <Button
+                            basic color='green'
+                            content='Add'
+                            onClick={this.handleAdd}
+                        />
+                        {
+                        this.state.counters.length === 0 ? "Empty" : this.state.counters.map(counter => (
+                            <Counter 
+                                key={counter.id}
+                                onDelete={this.handleDelete}
+                                onIncrement={this.handleIncrement}
+                                counter={counter}
+                            />
+                        ))
+                        }
+                    </div>
+                    }
+                </div>
         );
     }
 }
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,24 +2,8 @@ import React from 'react';
 import { Button, Label } from 'semantic-ui-react';
 
 class Counter extends React.Component {
-    state = {
-        value: this.props.value
-    }
-
-    handlePlus = () => {
-        this.setState({
-            value: this.state.value + 1
-        });
-    }
-
-    handleMinus = () => {
-        this.setState({
-            value: this.state.value - 1
-        });
-    }
-
     counterFormat = () => {
-        const { value } = this.state;
+        const { value } = this.props.counter;
         return value === 0 ? "ZERO" : value;
     }
 
@@ -28,14 +12,26 @@ class Counter extends React.Component {
     }
 
     render() {
-        let colors = this.state.value === 0 ? 'yellow' : 'black';
+        let colors = this.props.counter.value === 0 ? 'yellow' : 'black';
 
         return(
             <div>
                 <Label as='a' color={colors} tag>{this.counterFormat()}</Label>                
-                <Button style={this.styles} onClick={this.handlePlus}>+</Button>
-                <Button style={this.styles} onClick={this.handleMinus}>-</Button>
-                <Button basic color='red' onClick={() => this.props.onDelete(this.props.id)}>Delete</Button>
+                <Button
+                    style={this.styles}
+                    onClick={() => this.props.onIncrement(this.props.counter)}
+                >+
+                </Button>
+                <Button
+                    style={this.styles}
+                    onClick={this.handleMinus}
+                >-
+                </Button>
+                <Button
+                    basic color='red'
+                    onClick={() => this.props.onDelete(this.props.counter.id)}
+                >Delete
+                </Button>
             </div>
         );
     }
